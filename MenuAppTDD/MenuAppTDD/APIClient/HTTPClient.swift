b1965f0a6724d16cb3ce4f8ebd33356656f6c085
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-03-23 21:19:16 +0100

    Create URLSession data task with url on 'URLSessionHTTPClient.get(from: URL)'

---
+++ MenuAppTDD/MenuAppTDD/APIClient/HTTPClient.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPClient.swift
+//  MenuAppTDD
+//
+//  Created by Vinicius Moreira Leal on 23/03/2020.
+//  Copyright © 2020 Vinicius Leal and Gabriel Meira. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL) -> Result<[Category], Error>
+}
---
+++ MenuAppTDD/MenuAppTDD/Models/Category.swift
@@ -0,0 +1,19 @@
+//
+//  Category.swift
+//  MenuAppTDD
+//
+//  Created by Vinicius Moreira Leal on 10/02/2020.
+//  Copyright © 2020 Vinicius Leal and Gabriel Meira. All rights reserved.
+//
+
+import Foundation
+
+public struct Category: Codable {
+    let categoryID: String
+    let categoryName: String
+
+    public init(categoryID: String, categoryName: String) {
+        self.categoryID = categoryID
+        self.categoryName = categoryName
+    }
+}
