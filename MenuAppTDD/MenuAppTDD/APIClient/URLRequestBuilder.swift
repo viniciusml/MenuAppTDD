2b4c8200c379eda713c75a7eecfddbd6e1dda705
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-04-13 14:23:33 +0200

    Move URLRequest components out of the 'URLRequestBuilder'

--- MenuAppTDD/MenuAppTDD/APIClient/URLRequestBuilder.swift
+++ MenuAppTDD/MenuAppTDD/APIClient/URLRequestBuilder.swift
@@ -14,24 +14,32 @@ public protocol RequestBuilder {
     func buildRequest(for endpoint: Endpoint) -> URLRequest
 }
 
-public struct URLRequestBuilder: RequestBuilder {
+struct URLRequestComponent {
+    static let baseURL = "https://developers.zomato.com/api/v2.1"
     
-    private let baseURL = "https://developers.zomato.com/api/v2.1"
-    
-    private let defaultHeaders: [String: String] = [
+    static let defaultHeaders: [String: String] = [
         "Content-Type": "application/json",
         "user-key": "d25a516a7a59fb465b3b1440a2c92621"
     ]
+}
+
+public struct URLRequestBuilder: RequestBuilder {
     
-    public init() {}
+    private let urlString: String
+    private let headers: [String: String]
+    
+    public init(urlString: String, headers: [String: String]) {
+        self.urlString = urlString
+        self.headers = headers
+    }
         
     public func buildRequest(for endpoint: Endpoint = "/categories") -> URLRequest {
-        guard let url = URL(string: baseURL + endpoint ) else {
+        guard let url = URL(string: urlString + endpoint ) else {
             fatalError("Invalid URL")
         }
 
         var request = URLRequest(url: url)
-        request.allHTTPHeaderFields = defaultHeaders
+        request.allHTTPHeaderFields = headers
         return request
     }
 }
