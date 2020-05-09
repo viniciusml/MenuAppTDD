b5fc5d702d562c7c11a2eb4672748c8a9c230766
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-05-09 16:21:51 +0200

    Create new target for EndToEnd tests

---
+++ MenuAppTDD/MenuAppTDDAPIEndToEndTests/MenuAppTDDAPIEndToEndTests.swift
@@ -0,0 +1,13 @@
+//
+//  MenuAppTDDAPIEndToEndTests.swift
+//  MenuAppTDDAPIEndToEndTests
+//
+//  Created by Vinicius Moreira Leal on 09/05/2020.
+//  Copyright © 2020 Vinicius Leal and Gabriel Meira. All rights reserved.
+//
+
+import XCTest
+
+class MenuAppTDDAPIEndToEndTests: XCTestCase {
+
+}
